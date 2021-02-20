930293debc30f5c45575d62d75de8733170f3045
Saida5932 <79358921+Saida5932@users.noreply.github.com>
2021-02-20 21:16:26 +0300

    TheTastNumber9

---
+++ TheTaskNumber9/TheTaskNumber9.cpp
@@ -0,0 +1,35 @@
+﻿// TheTaskNumber9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	char* n;
+	char* next = 0;
+	char str[] = "- А что это за шаги такие на лестнице? - спросил Коровьев, поигрывая ложечкой в чашке с черным кофе.";
+
+	printf("%s", "Строка:\n");
+	printf(str);
+	printf("\n");
+	printf("%s", "Пословный вывод строки:\n");
+
+	n = strtok_s(str, " -,?.", &next);
+	do
+	{
+		printf("%s", n);
+		printf("\n", next);
+	} while (n = strtok_s(NULL, " -,?.", &next));
+
+	return 0;
+
+
+
+
+
+
+}
